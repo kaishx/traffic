bffd9868cc0a625746de1ae4962786719b23a897
kaishx <chukaile72@gmail.com>
2026-01-08 12:32:45 +0800

    .

--- brain/main.py
+++ brain/main.py
@@ -1,1 +1,7 @@
 # this is for running the simulations, we just import everything from other files to keep it clean.
+
+from cars.car import Car
+from brain.car_logic import CarBrain
+import pygame
+
+
